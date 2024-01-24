33c5e5a411de04d3dcdbd047ea83ef1a1bdc24b1
nadyaamalya <nadya01mei@gmail.com>
2024-01-24 15:07:02 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\HitungController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/smktelkompwt', function () {
+    return 'smktelkompwt';
+});
+
+
+Route::get('/user/{name?}', function ($name = 'nadya') {
+    return $name;
+});
+
+Route::get('/', function () {
+    return view ('welcome');
+})-> name('home');
+
+Route::get('/pendataan', function () {
+    return view ('pendataan');
+})-> name('pendataan');
+
+Route::get('/', [HitungController::class, 'hitung']);
+
+Route::get('/daftar', [TestController::class, 'daftar']);
+Route::post('/kirim', [TestController::class, 'kirim']);
+
